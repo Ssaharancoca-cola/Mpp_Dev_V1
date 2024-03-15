def11b52429c85ffc5ff72660ca8a2e8c6e49273
Amit Sharma <amitsharma07@coca-cola.com>
2024-03-15 06:27:25 +0530

    UI changes as per kartikeya feedback

---
+++ MPP/wwwroot/js/custome.js
@@ -0,0 +1,15 @@
+//Sidebar Open and close
+$(document).on('click', '.sidebarToggleBtn', function () {
+    $('body').toggleClass('sideOpen');
+})
+//Header Menu remove and add active class
+$(document).on('click', '.customNavbar .navbar-nav li', function () {
+    $('.customNavbar .navbar-nav li a').removeClass('active');
+    $(this).find('a').addClass('active');
+    $('body').addClass('sideOpen');
+});
+//Sidebar Menu remove and add active class
+$(document).on('click', '.mainSidebar ul li', function () {
+    $('.mainSidebar ul li a').removeClass('active');
+    $(this).find('a').addClass('active');
+});
