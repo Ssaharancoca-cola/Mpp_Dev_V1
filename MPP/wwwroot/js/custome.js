7c85323d8df0a7ece82b4cf8a9e5f287b1e21cf5
Amit Sharma <amitsharma07@coca-cola.com>
2024-04-10 10:09:51 +0530

    table sorting icon

--- MPP/wwwroot/js/custome.js
+++ MPP/wwwroot/js/custome.js
@@ -23,3 +23,39 @@ $(document).on('click', 'btn-group', function () {
     $('button').removeClass('active');
     $(this).find('button').addClass('active');
 });
+
+// Data Table Sorting icon 
+function addArrowIcons() {
+    var headers = document.querySelectorAll("#tblsearchdata th");
+    headers.forEach(function (header) {
+        var upIcon = document.createElement("i");
+        upIcon.classList.add("fas", "fa-arrow-up");
+        upIcon.style.display = "none";
+
+        var downIcon = document.createElement("i");
+        downIcon.classList.add("fas", "fa-arrow-down");
+
+        header.appendChild(upIcon);
+        header.appendChild(downIcon);
+
+        header.addEventListener("click", function () {
+            toggleArrowIcons(header);
+            /*sortTable(header.cellIndex);*/
+        });
+    });
+}
+
+function toggleArrowIcons(header) {
+    var arrowIcons = header.querySelectorAll("i");
+    arrowIcons.forEach(function (icon) {
+        icon.style.display = icon.style.display === "none" ? "inline-block" : "none";
+        icon.classList.toggle("active");
+    });
+
+
+}
+
+// Add arrow icons when the page loads
+window.onload = function () {
+    addArrowIcons();
+};
